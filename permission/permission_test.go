2f93bceea480308d3857c5745db849f0b8d45fae
Mzack9999 <mzack9999@protonmail.com>
2023-05-03 17:30:16 +0200

    fixing error

--- permission/permission_test.go
+++ permission/permission_test.go
@@ -1,18 +1,15 @@
+//go:build windows || linux
+
 package permissionutil
 
 import (
 	"testing"
 
-	osutils "github.com/projectdiscovery/utils/os"
 	"github.com/stretchr/testify/require"
 )
 
 func TestIsRoot(t *testing.T) {
-	t.Run("windows - linux", func(t *testing.T) {
-		isRoot, err := checkCurrentUserRoot()
-		if osutils.IsWindows() || osutils.IsLinux() {
-			require.Nil(t, err)
-			require.NotNil(t, isRoot)
-		}
-	})
+	isRoot, err := checkCurrentUserRoot()
+	require.Nil(t, err)
+	require.NotNil(t, isRoot)
 }
