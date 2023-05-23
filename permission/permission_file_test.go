f00e666f8aa6f65382f1461067aefcfea4d49047
shubhamrasal <shubhamdharmarasal@gmail.com>
2023-05-23 11:54:10 +0530

    Run tests only on linux and darwin

---
+++ permission/permission_file_test.go
@@ -0,0 +1,90 @@
+//go:build linux || darwin
+
+package permissionutil
+
+import (
+	"os"
+	"testing"
+)
+
+// run the file permission tests on linux and osx
+func TestFilePermissions(t *testing.T) {
+
+	t.Run("TestFileAllReadWriteExecute", func(t *testing.T) {
+		file, err := os.CreateTemp("", "testfile")
+		if err != nil {
+			t.Fatalf("Failed to create test file: %v", err)
+		}
+		testFileName := file.Name()
+		defer os.Remove(testFileName)
+		defer file.Close()
+
+		// Set the file permissions
+		err = file.Chmod(os.FileMode(AllReadWriteExecute))
+		if err != nil {
+			t.Fatalf("Failed to set file permissions: %v", err)
+		}
+
+		// Get the file permissions
+		fileInfo, err := os.Stat(testFileName)
+		if err != nil {
+			t.Fatalf("Failed to get file info: %v", err)
+		}
+		// Check if the file permissions match the defined constants
+		if fileInfo.Mode().Perm().String() != "-rwxrwxrwx" || fileInfo.Mode().Perm() != os.FileMode(AllReadWriteExecute) {
+			t.Errorf("File permissions do not match. Expected: %s, Actual: %s", os.FileMode(AllReadWriteExecute).String(), fileInfo.Mode().Perm().String())
+		}
+	})
+
+	t.Run("TestFileUserReadWriteExecute", func(t *testing.T) {
+		file, err := os.CreateTemp("", "testfile")
+		if err != nil {
+			t.Fatalf("Failed to create test file: %v", err)
+		}
+		testFileName := file.Name()
+		defer os.Remove(testFileName)
+		defer file.Close()
+
+		// Set the file permissions
+		err = file.Chmod(os.FileMode(UserReadWriteExecute))
+		if err != nil {
+			t.Fatalf("Failed to set file permissions: %v", err)
+		}
+
+		// Get the file permissions
+		fileInfo, err := os.Stat(testFileName)
+		if err != nil {
+			t.Fatalf("Failed to get file info: %v", err)
+		}
+		// Check if the file permissions match the defined constants
+		if fileInfo.Mode().Perm().String() != "-rwx------" || fileInfo.Mode().Perm() != os.FileMode(UserReadWriteExecute) {
+			t.Errorf("File permissions do not match. Expected: %s, Actual: %s", os.FileMode(UserReadWriteExecute).String(), fileInfo.Mode().Perm().String())
+		}
+	})
+
+	t.Run("TestFileGroupReadWriteExecute", func(t *testing.T) {
+		file, err := os.CreateTemp("", "testfile")
+		if err != nil {
+			t.Fatalf("Failed to create test file: %v", err)
+		}
+		testFileName := file.Name()
+		defer os.Remove(testFileName)
+		defer file.Close()
+
+		// Set the file permissions
+		err = file.Chmod(os.FileMode(UserReadWriteExecute | GroupReadWriteExecute))
+		if err != nil {
+			t.Fatalf("Failed to set file permissions: %v", err)
+		}
+
+		// Get the file permissions
+		fileInfo, err := os.Stat(testFileName)
+		if err != nil {
+			t.Fatalf("Failed to get file info: %v", err)
+		}
+		// Check if the file permissions match the defined constants
+		if fileInfo.Mode().Perm().String() != "-rwxrwx---" || fileInfo.Mode().Perm() != os.FileMode(UserReadWriteExecute|GroupReadWriteExecute) {
+			t.Errorf("File permissions do not match. Expected: %s, Actual: %s", os.FileMode(UserReadWriteExecute|GroupReadWriteExecute).String(), fileInfo.Mode().Perm().String())
+		}
+	})
+}
--- permission/permission_test.go
+++ permission/permission_test.go
@@ -3,7 +3,6 @@
 package permissionutil
 
 import (
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -13,85 +12,4 @@ func TestIsRoot(t *testing.T) {
 	isRoot, err := checkCurrentUserRoot()
 	require.Nil(t, err)
 	require.NotNil(t, isRoot)
-}
-
-func TestFilePermissions(t *testing.T) {
-
-	t.Run("TestFileAllReadWriteExecute", func(t *testing.T) {
-		file, err := os.CreateTemp("", "testfile")
-		if err != nil {
-			t.Fatalf("Failed to create test file: %v", err)
-		}
-		testFileName := file.Name()
-		defer os.Remove(testFileName)
-		defer file.Close()
-
-		// Set the file permissions
-		err = file.Chmod(os.FileMode(AllReadWriteExecute))
-		if err != nil {
-			t.Fatalf("Failed to set file permissions: %v", err)
-		}
-
-		// Get the file permissions
-		fileInfo, err := os.Stat(testFileName)
-		if err != nil {
-			t.Fatalf("Failed to get file info: %v", err)
-		}
-		// Check if the file permissions match the defined constants
-		if fileInfo.Mode().Perm().String() != "-rwxrwxrwx" || fileInfo.Mode().Perm() != os.FileMode(AllReadWriteExecute) {
-			t.Errorf("File permissions do not match. Expected: %s, Actual: %s", os.FileMode(AllReadWriteExecute).String(), fileInfo.Mode().Perm().String())
-		}
-	})
-
-	t.Run("TestFileUserReadWriteExecute", func(t *testing.T) {
-		file, err := os.CreateTemp("", "testfile")
-		if err != nil {
-			t.Fatalf("Failed to create test file: %v", err)
-		}
-		testFileName := file.Name()
-		defer os.Remove(testFileName)
-		defer file.Close()
-
-		// Set the file permissions
-		err = file.Chmod(os.FileMode(UserReadWriteExecute))
-		if err != nil {
-			t.Fatalf("Failed to set file permissions: %v", err)
-		}
-
-		// Get the file permissions
-		fileInfo, err := os.Stat(testFileName)
-		if err != nil {
-			t.Fatalf("Failed to get file info: %v", err)
-		}
-		// Check if the file permissions match the defined constants
-		if fileInfo.Mode().Perm().String() != "-rwx------" || fileInfo.Mode().Perm() != os.FileMode(UserReadWriteExecute) {
-			t.Errorf("File permissions do not match. Expected: %s, Actual: %s", os.FileMode(UserReadWriteExecute).String(), fileInfo.Mode().Perm().String())
-		}
-	})
-
-	t.Run("TestFileGroupReadWriteExecute", func(t *testing.T) {
-		file, err := os.CreateTemp("", "testfile")
-		if err != nil {
-			t.Fatalf("Failed to create test file: %v", err)
-		}
-		testFileName := file.Name()
-		defer os.Remove(testFileName)
-		defer file.Close()
-
-		// Set the file permissions
-		err = file.Chmod(os.FileMode(UserReadWriteExecute | GroupReadWriteExecute))
-		if err != nil {
-			t.Fatalf("Failed to set file permissions: %v", err)
-		}
-
-		// Get the file permissions
-		fileInfo, err := os.Stat(testFileName)
-		if err != nil {
-			t.Fatalf("Failed to get file info: %v", err)
-		}
-		// Check if the file permissions match the defined constants
-		if fileInfo.Mode().Perm().String() != "-rwxrwx---" || fileInfo.Mode().Perm() != os.FileMode(UserReadWriteExecute|GroupReadWriteExecute) {
-			t.Errorf("File permissions do not match. Expected: %s, Actual: %s", os.FileMode(UserReadWriteExecute|GroupReadWriteExecute).String(), fileInfo.Mode().Perm().String())
-		}
-	})
 }
