36166b3afa6b5aa1f58782a12657bb1747836752
Doğan Can Bakır <dogancanbakir@protonmail.com>
2023-05-25 16:41:52 +0300

    add common regex patterns

---
+++ patterns/patterns.go
@@ -0,0 +1,35 @@
+package patterns
+
+import "regexp"
+
+const (
+	IPv4          string = `^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$`
+	IPv6          string = `(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))`
+	URLSCHEMA     string = `((ftp|tcp|udp|wss?|https?):\/\/)`
+	URLUSERNAME   string = `(\S+(:\S*)?@)`
+	URLPATH       string = `((\/|\?|#)[^\s]*)`
+	URLPORT       string = `(:(\d{1,5}))`
+	URLSUBDOMAIN  string = `((www\.)|([a-zA-Z0-9]+([-_\.]?[a-zA-Z0-9])*[a-zA-Z0-9]\.[a-zA-Z0-9]+))`
+	URL                  = `^` + URLSCHEMA + `?` + URLUSERNAME + `?` + `((` + IPv4 + `|(\[` + IPv6 + `\])|(([a-zA-Z0-9]([a-zA-Z0-9-_]+)?[a-zA-Z0-9]([-\.][a-zA-Z0-9]+)*)|(` + URLSUBDOMAIN + `?))?(([a-zA-Z\x{00a1}-\x{ffff}0-9]+-?-?)*[a-zA-Z\x{00a1}-\x{ffff}0-9]+)(?:\.([a-zA-Z\x{00a1}-\x{ffff}]{1,}))?))\.?` + URLPORT + `?` + URLPATH + `?$`
+	URLPORT80     string = `^.*:80$`
+	URLPORT443    string = `^.*:443$`
+	DOMAIN               = `^(?i)[a-z0-9-]+(\.[a-z0-9-]+)+?$`
+	EMAIL         string = "^(((([a-zA-Z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])+(\\.([a-zA-Z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])+)*)|((\\x22)((((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(([\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x7f]|\\x21|[\\x23-\\x5b]|[\\x5d-\\x7e]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(\\([\\x01-\\x09\\x0b\\x0c\\x0d-\\x7f]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}]))))*(((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(\\x22)))@((([a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(([a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])([a-zA-Z]|\\d|-|\\.|_|~|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])*([a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])))\\.)+(([a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(([a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])([a-zA-Z]|\\d|-|_|~|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])*([a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])))\\.?$"
+	CIDR          string = `^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?|[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?|[0-9])\/([1-2][0-9]|3[0-2]|[0-9])$`
+	CVEIDENTIFIER string = `(?i)cve-\d{4}-\d{4,}$`
+	DUALSPACE     string = `\s+`
+	DECOLORIZER   string = `\x1B\[[0-9;]*[a-zA-Z]`
+)
+
+var (
+	URLRegexp           = regexp.MustCompile(URL)
+	DomainRegexp        = regexp.MustCompile(DOMAIN)
+	IPv4Regexp          = regexp.MustCompile(IPv4)
+	URLPort80Regexp     = regexp.MustCompile(URLPORT80)
+	URLPort443Regexp    = regexp.MustCompile(URLPORT443)
+	EmailRegexp         = regexp.MustCompile(EMAIL)
+	CIDRRegexp          = regexp.MustCompile(CIDR)
+	CVEIdentifierRegexp = regexp.MustCompile(CVEIDENTIFIER)
+	DualSpaceRegexp     = regexp.MustCompile(DUALSPACE)
+	DecolorizerRegexp   = regexp.MustCompile(DECOLORIZER)
+)
