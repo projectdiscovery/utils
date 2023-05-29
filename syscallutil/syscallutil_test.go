a4943bd1a8dbff9ff0945dbd793b47d2c5f61f86
Doğan Can Bakır <dogancanbakir@protonmail.com>
2023-05-29 19:40:01 +0300

    add darwin case to test

--- syscallutil/syscallutil_test.go
+++ syscallutil/syscallutil_test.go
@@ -1,6 +1,7 @@
 package syscallutil
 
 import (
+	"fmt"
 	"runtime"
 	"testing"
 
@@ -10,10 +11,15 @@ import (
 func TestLoadLibrary(t *testing.T) {
 	t.Run("Test valid library", func(t *testing.T) {
 		var lib string
-		if runtime.GOOS == "windows" {
+		switch runtime.GOOS {
+		case "windows":
 			lib = "ucrtbase.dll"
-		} else {
+		case "darwin":
+			lib = "libSystem.dylib"
+		case "linux":
 			lib = "libc.so.6"
+		default:
+			panic(fmt.Errorf("GOOS=%s is not supported", runtime.GOOS))
 		}
 
 		_, err := LoadLibrary(lib)
@@ -22,10 +28,15 @@ func TestLoadLibrary(t *testing.T) {
 
 	t.Run("Test invalid library", func(t *testing.T) {
 		var lib string
-		if runtime.GOOS == "windows" {
+		switch runtime.GOOS {
+		case "windows":
 			lib = "C:\\path\\to\\invalid\\library.dll"
-		} else {
+		case "darwin":
+			lib = "/path/to/invalid/library.dylib"
+		case "linux":
 			lib = "/path/to/invalid/library.so"
+		default:
+			panic(fmt.Errorf("GOOS=%s is not supported", runtime.GOOS))
 		}
 
 		_, err := LoadLibrary(lib)
