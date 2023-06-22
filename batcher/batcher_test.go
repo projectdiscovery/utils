6c3f22bc924044f93993924a0c428a7d881aa6ab
mzack <marco.rivoli.nvh@gmail.com>
2023-06-22 09:23:02 +0200

    adding test with interval

--- batcher/batcher_test.go
+++ batcher/batcher_test.go
@@ -2,11 +2,12 @@ package batcher
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestBatcher(t *testing.T) {
+func TestBatcherStandard(t *testing.T) {
 	var (
 		batchSize        = 100
 		wanted           = 100000
@@ -28,6 +29,41 @@ func TestBatcher(t *testing.T) {
 	bat.Run()
 
 	for i := 0; i < wanted; i++ {
+		bat.Append(i)
+	}
+
+	bat.Stop()
+
+	bat.WaitDone()
+
+	require.Equal(t, wanted, got)
+	require.True(t, minWantedBatches <= gotBatches)
+}
+
+func TestBatcherWithInterval(t *testing.T) {
+	var (
+		batchSize        = 200
+		wanted           = 1000
+		minWantedBatches = 10
+		got              int
+		gotBatches       int
+	)
+	callback := func(t []int) {
+		gotBatches++
+		for range t {
+			got++
+		}
+	}
+	bat := New[int](
+		WithMaxCapacity[int](batchSize),
+		WithFlushCallback[int](callback),
+		WithFlushInterval[int](10*time.Millisecond),
+	)
+
+	bat.Run()
+
+	for i := 0; i < wanted; i++ {
+		time.Sleep(2 * time.Millisecond)
 		bat.Append(i)
 	}
 
