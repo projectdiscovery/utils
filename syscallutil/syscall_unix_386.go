d338c0318fdbf266cc4b68c7f9c5ac5cb32e65cf
mzack <marco.rivoli.nvh@gmail.com>
2023-07-14 16:09:55 +0200

    adding unix 386 fallback

--- syscallutil/syscall_unix_386.go
+++ syscallutil/syscall_unix_386.go
@@ -1,9 +1,9 @@
-//go:build (darwin || linux) && !386
+//go:build (darwin || linux) && 386
 
 package syscallutil
 
 import "github.com/ebitengine/purego"
 
 func loadLibrary(name string) (uintptr, error) {
-	return purego.Dlopen(name, purego.RTLD_NOW|purego.RTLD_GLOBAL)
+	return -1, errors.New("not implemented")
 }
