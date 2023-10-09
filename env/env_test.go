6bc0e3c0d3316d33c38bc86a4573effa4254a7b6
Tarun Koyalwar <tarun@projectdiscovery.io>
2023-10-09 20:46:19 +0530

    add pprof utils projectdiscovery/nuclei#4212

--- env/env_test.go
+++ env/env_test.go
@@ -3,6 +3,7 @@ package env
 import (
 	"os"
 	"testing"
+	"time"
 )
 
 func TestExpandWithEnv(t *testing.T) {
@@ -41,4 +42,47 @@ func TestExpandWithEnvNilInput(t *testing.T) {
 
 	var nilVar *string = nil
 	ExpandWithEnv(nilVar)
+}
+
+func TestGetEnvOrDefault(t *testing.T) {
+	// Test for string
+	os.Setenv("TEST_STRING", "test")
+	resultString := GetEnvOrDefault("TEST_STRING", "default")
+	if resultString != "test" {
+		t.Errorf("Expected 'test', got %s", resultString)
+	}
+
+	// Test for int
+	os.Setenv("TEST_INT", "123")
+	resultInt := GetEnvOrDefault("TEST_INT", 0)
+	if resultInt != 123 {
+		t.Errorf("Expected 123, got %d", resultInt)
+	}
+
+	// Test for bool
+	os.Setenv("TEST_BOOL", "true")
+	resultBool := GetEnvOrDefault("TEST_BOOL", false)
+	if resultBool != true {
+		t.Errorf("Expected true, got %t", resultBool)
+	}
+
+	// Test for float64
+	os.Setenv("TEST_FLOAT", "1.23")
+	resultFloat := GetEnvOrDefault("TEST_FLOAT", 0.0)
+	if resultFloat != 1.23 {
+		t.Errorf("Expected 1.23, got %f", resultFloat)
+	}
+
+	// Test for time.Duration
+	os.Setenv("TEST_DURATION", "1h")
+	resultDuration := GetEnvOrDefault("TEST_DURATION", time.Duration(0))
+	if resultDuration != time.Hour {
+		t.Errorf("Expected 1h, got %s", resultDuration)
+	}
+
+	// Test for default value
+	resultDefault := GetEnvOrDefault("NON_EXISTING", "default")
+	if resultDefault != "default" {
+		t.Errorf("Expected 'default', got %s", resultDefault)
+	}
 }
