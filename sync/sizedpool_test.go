0adf3356f41a5ff2686d4155512033ea956b321e
mzack <marco.rivoli.nvh@gmail.com>
2024-03-08 13:27:28 +0100

    tests

--- sync/sizedpool_test.go
+++ sync/sizedpool_test.go
@@ -29,8 +29,13 @@ func TestSizedPool(t *testing.T) {
 
 	// Test Get and Put operations
 	ctx := context.Background()
-	obj1, _ := pool.Get(ctx)
-	obj2, _ := pool.Get(ctx)
+	obj1, err := pool.Get(ctx)
+	require.Nil(t, err)
+	require.NotNil(t, obj1)
+
+	obj2, err := pool.Get(ctx)
+	require.Nil(t, err)
+	require.NotNil(t, obj2)
 
 	go func() {
 		time.Sleep(3 * time.Second)
@@ -43,4 +48,47 @@ func TestSizedPool(t *testing.T) {
 	obj3, _ := pool.Get(ctx)
 	require.WithinDuration(t, start.Add(3*time.Second), time.Now(), 500*time.Millisecond)
 	require.NotNil(t, obj3)
+}
+
+func TestSizedPoolVary(t *testing.T) {
+	p := &sync.Pool{
+		New: func() any {
+			return &testStruct{}
+		},
+	}
+
+	// Create a new SizedPool with a max capacity of 2
+	pool, err := New[*testStruct](
+		WithSize[*testStruct](2),
+		WithPool[*testStruct](p),
+	)
+	if err != nil {
+		t.Errorf("Error creating pool: %v", err)
+	}
+
+	// Test Get and Put operations
+	ctx := context.Background()
+	obj1, err := pool.Get(ctx)
+	require.Nil(t, err)
+	require.NotNil(t, obj1)
+
+	obj2, err := pool.Get(ctx)
+	require.Nil(t, err)
+	require.NotNil(t, obj2)
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		obj3, err := pool.Get(context.Background())
+		require.Nil(t, err)
+		require.NotNil(t, obj3)
+	}()
+
+	err = pool.Vary(context.Background(), 1)
+	require.Nil(t, err)
+
+	wg.Wait()
 }
