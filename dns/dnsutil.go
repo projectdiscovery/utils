fa7109358d31ac6a9cc8a2bd431ab3fd725cc5ca
Doğan Can Bakır <dogancanbakir@protonmail.com>
2024-09-16 11:24:31 +0300

    improve comment

--- dns/dnsutil.go
+++ dns/dnsutil.go
@@ -5,8 +5,12 @@ import (
 	"github.com/weppos/publicsuffix-go/publicsuffix"
 )
 
-// DecomposeDomain decomposes a given domain into subdomain and domain.
-// It returns the subdomain and the domain.
+// DecomposeDomain takes a domain name and decomposes it into its subdomain and domain components.
+// The function returns the subdomain, the domain, and an error if the decomposition process fails.
+//
+// For example:
+//   - Input: "http://www.example.com"
+//   - Output: "www", "example.com", nil
 func DecomposeDomain(name string) (string, string, error) {
 	name = stringsutil.TrimPrefixAny(name, "http://", "https://")
 	dn, err := publicsuffix.ParseFromListWithOptions(publicsuffix.DefaultList, name, publicsuffix.DefaultFindOptions)
